da8de4ea689e3043f29517017d1b47b5fe1d8c81
shubham-Alhat <shubhamalhat520@gmail.com>
2026-01-07 22:22:34 +0530

    prisma setup done01

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json({ limit: "60kb" }));
+app.use(express.urlencoded({ extended: true, limit: "60kb" }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+export default app;
